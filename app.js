88f129d4ea7061736cd36c778f220b8d25176439
weijie <qoo77823@hotmail.com>
2024-02-11 21:57:50 +0800

    install express-handlebars, and init seqelize

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict'
+
+//express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// express-handlebars
+const { engine } = require('express-handlebars');
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// middleware
+// static file
+app.use(express.static('public'))
