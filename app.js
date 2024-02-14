ff443f25f22d4fd8dadc153da6c9c120be11c6e9
weijie <qoo77823@hotmail.com>
2024-02-14 20:37:00 +0800

    feat: add view engein page

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ const port = 3000;
 const { engine } = require('express-handlebars');
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
+app.set('views', './views')
 
 // middleware
 // static file
 app.use(express.static('public'))
+
+// router
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// listen
+app.listen(port, () => {
+  console.log(`express server on http://localhost:${port}`)
+})
